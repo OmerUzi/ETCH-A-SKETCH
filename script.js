7d139c5d41f66f808e43c89db32c11511e8ec490
omer uzi <omer135798@gmail.com>
2021-09-22 18:06:10 +0300

    finish the button function

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
-function createFlexbox() 
-{
-    const container = document.createElement("div");
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+let container;
+
+createFlexbox(16);
+button.addEventListener("click" , buttonFunction);
+
+function createFlexbox(x)
+{   
+    container = document.createElement("div")
     container.id = "container";
     container.style.display = "flex";
     container.style.flexWrap = "wrap";
+    container.style.width = "100vw";
 
     let square;
-    for(let i = 0; i < 256; i++){
+    let side = 100/x;
+    side.toString();
+    side = side + "vw";
+
+    for(let i = 0; i < x*x; i++){
         square = document.createElement("div");
-        square.style.width = "6vw";
-        square.style.height = "6vw";
+        square.style.width = side;
+        square.style.height = side;
         square.id = "div" + i.toString();
         square.className = "square-divs";
         container.appendChild(square);
         square.addEventListener("mouseover",(e) => e.target.classList.add("hover-onced"));
     }
 
-    const body = document.querySelector("body");
     body.appendChild(container);
 
 }
-function clearAll()
+
+function buttonFunction()
 {
     let divs = document.querySelectorAll("div.hover-onced");
     divs.forEach(div => {
     div.classList.remove("hover-onced");
-  });
+    });
+    let text = prompt('how much you want the grid?\n(enter number between 1-100?');
+    while (text>100 || text < 1){
+        text = prompt('number is not between 1-100?');
+    }
+    text= parseInt(text);
+    
+    container = document.querySelector('#container')
+    body.removeChild(container);
+    createFlexbox(text);
+
 }
-createFlexbox();
-const button = document.querySelector("button");
-button.addEventListener("click" , clearAll);
- 
 
 
 
-//square.style.display = "inline-block";
